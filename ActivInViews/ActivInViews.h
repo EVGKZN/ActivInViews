96a96f6a0cf51b5235b1bbc3836b5b4c829a934f
Evgenii Kuzmin <kuzmin@technokratos.com>
2021-04-02 18:38:23 +0300

    Initial Commit

---
+++ ActivInViews/ActivInViews.h
@@ -0,0 +1,18 @@
+//
+//  ActivInViews.h
+//  ActivInViews
+//
+//  Created by Евгений Кузьмин on 02.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ActivInViews.
+FOUNDATION_EXPORT double ActivInViewsVersionNumber;
+
+//! Project version string for ActivInViews.
+FOUNDATION_EXPORT const unsigned char ActivInViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActivInViews/PublicHeader.h>
+
+
